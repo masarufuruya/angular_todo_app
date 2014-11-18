a05c1149091b5b382c382cde73b0d4a01538b056
Masaru Furuya <m.furuya@comnico.jp>
2014-11-18 13:04:14 +0900

    loading追加

--- cakephp/app/View/Top/index.ctp
+++ cakephp/app/View/Top/index.ctp
@@ -24,28 +24,31 @@
   <body ng-app="HabbitApp">
     <div class="container">
       <div ng-controller="HabbitListCtrl">
-        <div id="addForm" class="row">
+        <div id="AddForm" class="row">
           <form ng-submit="addTodo()">
             <div id="AddInputBox" class="form-group">
-              <div class="col-sm-10">
+              <div class="col-sm-11">
                 <input type="text"  ng-model="name" class="form-control" />
               </div>
             </div>
           </form>
-          <div class="col-sm-2">
+          <div class="col-sm-1">
             <button ng-click="addTodo()" class="btn btn-primary">追加</button>
           </div>
         </div>
 
         <div id="HabbitList">
-          <table class="table table-striped">
+          <div id="HabbitListLoading" ng-show="isTodoListLoading == true">
+            <?php echo $this->Html->image('loading-circle.gif'); ?>
+          </div>
+          <table class="table table-striped" ng-show="isTodoListLoading == false">
               <tr>
                   <th></th>
-                  <th>ToDo</th>
+                  <th>習慣</th>
                   <th></th>
               </tr>
               <tr ng-repeat="todo in todos">
-                  <td class="col-sm-1"><input type="checkbox" ng-model="todo.done" width="10px"></td>
+                  <td class="col-sm-1"><input type="checkbox" ng-click="toggleIsDone($index)" ng-model="todo.isDone" width="10px"></td>
                   <td class="col-sm-10" ng-click="toCanEdit($index)">
                     <span ng-model="todo.label" ng-show="todo.isEdit == false">{{todo.message}}</span>
                     <input type="text" ng-blur="updateTodo($index)" ng-model="todo.message" ng-mouseleave="toNotEdit($index)" ng-show="todo.isEdit == true" class="form-control">
--- cakephp/app/webroot/js/index.js
+++ cakephp/app/webroot/js/index.js
@@ -2,17 +2,22 @@ var habbitApp = angular.module('HabbitApp', []);
 
 habbitApp.controller('HabbitListCtrl', function ($scope, $timeout, $http) {
 
-     $http({method: 'GET', url: '/top/todos'}).
-       success(function(data, status, headers, config) {
-         // レスポンスが有効の場合に、非同期で呼び出されるコールバックです。
-         $scope.todos = data;
-       }).
-       error(function(data, status, headers, config) {
+      // Todoリストローディング
+      $scope.isTodoListLoading = true;
+
+      $http({method: 'GET', url: '/top/todos'}).
+        success(function(data, status, headers, config) {
+          // レスポンスが有効の場合に、非同期で呼び出されるコールバックです。
+          $scope.todos = data;
+          $scope.isTodoListLoading = false;
+        }).
+        error(function(data, status, headers, config) {
          // エラーが発生、またはサーバからエラーステータスが返された場合に、
          // 非同期で呼び出されます。
-     });
+          $scope.isTodoListLoading = false;
+        });
 
-     $scope.addTodo = function() {
+      $scope.addTodo = function() {
           if ($scope.name !== '') {
                var name = $scope.name;
                $scope.name = '';
@@ -31,12 +36,12 @@ habbitApp.controller('HabbitListCtrl', function ($scope, $timeout, $http) {
                     //失敗
                });
           }
-     };
+      };
 
-     $scope.toCanEdit = function(index) {
+      $scope.toCanEdit = function(index) {
           $scope.todos[index].isEdit = true;
-     };
-     $scope.toNotEdit = function(index) {
+      };
+      $scope.toNotEdit = function(index) {
           // デフォルトだとmouseroverの処理が早いので、0.3秒遅延させる
           $timeout(function() {
                $scope.todos[index].isEdit = false;
@@ -46,9 +51,13 @@ habbitApp.controller('HabbitListCtrl', function ($scope, $timeout, $http) {
           if ($scope.todos[index].message !== '') {
                $scope.todos[index].label = $scope.todoMessage;
           }
-     };
+      };
 
-     $scope.updateTodo = function(index) {
+      $scope.toggleIsDone = function(index) {
+
+      }
+
+      $scope.updateTodo = function(index) {
           var updateTodo = $scope.todos[index];
           var parameter = {'id':updateTodo.id,'message':updateTodo.message};
 
@@ -62,9 +71,9 @@ habbitApp.controller('HabbitListCtrl', function ($scope, $timeout, $http) {
           }).error(function(data, status, headers, config) {
                //失敗
           });
-     };
+      };
 
-     $scope.deleteTodo = function(index) {
+      $scope.deleteTodo = function(index) {
 
           var deleteTodo = $scope.todos[index];
 
@@ -78,7 +87,7 @@ habbitApp.controller('HabbitListCtrl', function ($scope, $timeout, $http) {
           });
 
           $scope.todos.splice(index, 1);
-     };
+      };
 });
 
 
